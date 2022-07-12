f929e3d06abd0585dff8e526759f5b0729a5c150
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-11 20:25:53 -0400

    Fix Sidebar Styling Issues

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -22,14 +22,14 @@ const Sidebar = () => {
   return (
     <div>
       <div 
-      className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+      className='block xl:hidden m-2 ml-4 mt-3 text-xl'
       onClick={() => setShowSidebar((prev) => !prev)}
       >
         {showSidebar ? <ImCancelCircle/> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className="x1:w-400w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
-          <div className="x1:border-b-2 border-gray-200 xl:pb-4">
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 '>
+        <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
             <Link href='/'>
               <div className={normalLink}>
                 <p className="text-2xl">
