8f9d13fce5880433510bed4dfd044ec12aa1bd6b
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 01:27:55 -0400

    Add allUsers store field && setAllUsers function

--- store/authStore.ts
+++ store/authStore.ts
@@ -7,12 +7,14 @@ import axios from 'axios'
 
 const authStore = (set: any) => ({
     userProfile:null,
+    allUsers: [],
 
     addUser: (user:any) => set({userProfile: user}),
     removeUser: () => set({userProfile: null}),
 
     fetchAllUsers: async () => {
       const response = await axios.get(`${BASE_URL}/api/users`)
+      set({ allUsers: response.data})
     }
 })
 
