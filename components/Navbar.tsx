b4d52f74d7673657fa9d0ef01bd5de0c075723b2
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 05:39:14 -0400

    Fix Navbar styling + refactoring

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect} from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Router, { useRouter } from 'next/router'
-import { AiOutlineLogout } from 'react-icons/ai'
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
-import { BiSearch } from 'react-icons/bi'
-import { IoMdAdd } from 'react-icons/io'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout  } from '@react-oauth/google';
 
-import Logo from '../utils/tikmock-logo.png'
-import { createOrGetUser } from '../utils'
-
-import useAuthStore from '../store/authStore'
+import useAuthStore from '../store/authStore';
 import { IUser } from '../types';
+import { createOrGetUser } from '../utils';
+import Logo from '../utils/tikmock-logo.png';
 
 const Navbar = () => {
   const [user, setUser] = useState<IUser | null>();
   const [searchValue, setSearchValue] = useState('');
   const router = useRouter();
   const { userProfile, addUser, removeUser } = useAuthStore();
-
+  
   useEffect(() => {
     setUser(userProfile);
   }, [userProfile]);
@@ -32,14 +31,14 @@ const Navbar = () => {
   };
 
   return (
-    <div className ="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
-      <Link href="/">
-        <div className="w-[100px] md:w-[130px]">
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
           <Image
-            className="cursor-pointer"
+            className='cursor-pointer'
             src={Logo}
-            alt="TikMock"
-            layout="responsive"
+            alt='logo'
+            layout='responsive'
           />
         </div>
       </Link>
@@ -63,52 +62,48 @@ const Navbar = () => {
           </button>
         </form>
       </div>
-
       <div>
-        {userProfile ? (
-          <div className="flex gap-5 md:gap-10">
-            <Link href="/upload">
-              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl" /> {` `}
-                <span className="hidden md:block">Upload</span>
+        {user ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href='/upload'>
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl' />{' '}
+                <span className='hidden md:block'>Upload </span>
               </button>
             </Link>
-            {userProfile.image && (
-                <Link href="/">
-                <>
+            {user.image && (
+              <Link href={`/profile/${user._id}`}>
+                <div>
                   <Image
+                    className='rounded-full cursor-pointer'
+                    src={user.image}
+                    alt='user'
                     width={40}
                     height={40}
-                    className="rounded-full cursor-pointer"
-                    src={userProfile.image}
-                    alt="profile photo"
-                  ></Image>
-                </>
+                  />
+                </div>
               </Link>
             )}
-            <button
-            type="button"
-            className="px-2"
-            onClick={() => {
-              googleLogout()
-              removeUser()
-            }}
-            >
-              <AiOutlineLogout color="red" fontSize={21} />
-            </button>
+              <button
+                type='button'
+                className=' border-2 p-2 rounded-full cursor-pointer outline-none'
+                onClick={() => {
+                  googleLogout();
+                  removeUser();
+                }}
+              >
+                <AiOutlineLogout color='red' fontSize={21} />
+              </button>
           </div>
         ) : (
-          <GoogleLogin
-          onSuccess={(response) => {
-            createOrGetUser(response, addUser)
-          }}
-          onError={()=> {console.log('Error')}}
-          />
+            <GoogleLogin
+              onSuccess={(response) => createOrGetUser(response, addUser)}
+              onError={() => console.log('Login Failed')}
+            />
         )}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
