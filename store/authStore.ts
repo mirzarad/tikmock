a5a599d838ae9341a03647e62d41bcbd699fefd0
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 03:16:11 -0400

    Remove faulty import

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,22 +1,22 @@
 import create from 'zustand'
 import {persist} from 'zustand/middleware'
-import axois from 'axios'
-
-import { BASE_URL } from '../utils/'
 import axios from 'axios'
+import { BASE_URL } from '../utils/'
+
 
 const authStore = (set: any) => ({
-    userProfile:null,
-    allUsers: [],
+  userProfile: null,
+  allUsers: [],
+  
+  addUser: (user: any) => set({ userProfile: user }),
+  removeUser: () => set({ userProfile: null }),
 
-    addUser: (user:any) => set({userProfile: user}),
-    removeUser: () => set({userProfile: null}),
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
 
-    fetchAllUsers: async () => {
-      const response = await axios.get(`${BASE_URL}/api/users`)
-      set({ allUsers: response.data})
-    }
-})
+    set({ allUsers: response.data });
+  },
+});
 
 const useAuthStore = create(
   persist(authStore, {
