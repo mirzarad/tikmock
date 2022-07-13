27d9e3b345a24b516ecf2d7f6f19ce1434ec192e
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 05:41:30 -0400

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,29 +9,31 @@ interface IProps {
 }
 
 const Home = ({ videos }: IProps) => {
-  console.log(videos)
-
   return (
-    <div className="flex flex-col gap-10 videos h-full">
-      {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id}/>
-        ))
-      ) : (
-        <NoResults text={'No Videos'}/>
-      )}
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
     </div>
-  )
-}
+  );
+};
 
-export const getServerSideProps = async () => {
-  const {data} = await axios.get(`${BASE_URL}/api/post`)
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
 
-  return {
-    props: {
-      videos: data
-    }
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
   }
-}
+  
+  return {
+    props: { videos: response.data },
+  };
+};
 
 export default Home
