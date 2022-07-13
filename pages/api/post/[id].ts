00fd80a2a0c84680ba1d9e34cd35aa970de8d43f
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 03:14:04 -0400

    Fix code format

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -4,34 +4,31 @@ import { client } from '../../../utils/client'
 import { uuid } from 'uuidv4'
 import { postDetailQuery } from '../../../utils/queries'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse) {
-  if(req.method === 'GET'){
-    
-    const { id } = req.query
-    const query = postDetailQuery(id)
+export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const { id } = req.query;
+    const query = postDetailQuery(id);
 
-    const data = await client.fetch(query)
+    const data = await client.fetch(query);
 
-    res.status(200).json(data[0])
+    res.status(200).json(data[0]);
+  } else if (req.method === 'PUT') {
+    const { comment, userId } = req.body;
 
-  } else if(req.method === 'PUT'){
-    const { comment, userId} = req.body
-    const { id }:any = req.query
+    const { id }: any = req.query;
 
     const data = await client
       .patch(id)
-      .setIfMissing( { comments:[] })
+      .setIfMissing({ comments: [] })
       .insert('after', 'comments[-1]', [
-      {
-        comment,
-        _key: uuid(),
-        postedBy: { _type: 'postedBy', _ref: userId}
-      }
-    ]).commit()
+        {
+          comment,
+          _key: uuid(),
+          postedBy: { _type: 'postedBy', _ref: userId },
+        },
+      ])
+      .commit();
 
-    res.status(200).json(data)
+    res.status(200).json(data);
   }
-
 }
