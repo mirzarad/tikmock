7e90a0773df5e89f76672572bb08a1520ab536d8
Mirza Radoncic <mirzaradoncic@gmail.com>
2022-07-13 06:12:45 -0400

    Add link to favicon render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Head from 'next/head'
 import VideoCard from '../components/VideoCard'
 import NoResults from '../components/NoResults'
 import { Video } from '../types'
@@ -11,6 +12,11 @@ interface IProps {
 const Home = ({ videos }: IProps) => {
   return (
     <div className='flex flex-col gap-10 videos h-full'>
+      <Head>
+        <title>TikMock</title>
+        <meta name="TikMok" content="TikMok - TikTok Mock" />
+        <link rel="icon" href="../favicon.ico" />
+      </Head>
       {videos.length 
         ? videos?.map((video: Video) => (
           <VideoCard post={video} isShowingOnHome key={video._id} />
